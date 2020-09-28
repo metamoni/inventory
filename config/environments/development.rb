6b2ac4c4370b451ee16d5e035bea52362b3e8572
Monica Mateiu <monica.mateiu@gmail.com>
2020-09-28 07:50:11 +0100

    add inventory controller and styles

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,4 +61,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.sass.preferred_syntax = :sass
+  config.sass.syntax = :sass
 end
